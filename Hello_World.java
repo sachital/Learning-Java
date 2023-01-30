c27d1fc62fcf146bf1dff80ee3d7a73629d5e9cc
sachital <sachinmaurya1710@gmail.com>
2023-01-31 02:10:43 +0530

    Hello World boilerplate code

---
+++ Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World { //boilerplate code
+    public static void main(String args []) {
+        System.out.println(" Hello World!");// ln for next line
+        System.out.print(" Hello World\n");//\n for next line
+        System.out.print(" Hello World!");
+    }
+}
