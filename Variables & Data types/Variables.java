e54f9ecb3ff0daec3b88d259ba9f60e8ee5acbbf
sachital <sachinmaurya1710@gmail.com>
2023-02-06 19:46:14 +0530

    Added Variables & Data types

---
+++ Variables & Data types/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 16;
+        String name = "Danish";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+    }
+}
