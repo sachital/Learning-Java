22f6b7b72bf31320801eeea9f3135535fdf57150
sachital <sachinmaurya1710@gmail.com>
2023-02-08 09:48:32 +0530

    updated Data types

---
+++ 1. Variables & Data types/datatypes.java
@@ -0,0 +1,33 @@
+public class datatypes {
+    public static void main(String[] args) {
+        
+        //PRIMITIVE DATA TYPES
+        
+        byte d= 108;// byte has limited range upto 256 number 
+        System.out.println(d);
+        
+        char a = 'f';//char can store any character but only one i.e. 'a','b'& not 'ab','abc'
+        System.out.println(a);
+        
+        boolean var = true;// boolean var can only store true & false. used to check conditions.
+        System.out.println(var);
+        
+        float price = 1.28f;
+        /*float is used to store decimal entries and data types.
+        here in float (float)x or xf is used to execute the float function
+        where x is the value of data */
+        System.out.println(price);
+        
+        int danish=100;//int for any integer entry
+        System.out.println(danish);
+        
+        long ashish= 9515422;// long is used for long int inputs
+        System.out.println(ashish);
+        
+        double tax = 3.0654561;// double is used to print long float values
+        System.out.println(tax);
+        
+        short n = 562;
+        System.out.println(n);
+    }
+}
---
+++ 1. Variables & Data types/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    //code to calculate sum of two numbers
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 15;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
