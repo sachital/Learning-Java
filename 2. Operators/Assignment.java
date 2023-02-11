9b597d2b7f1783c49b5e59483694286b99833778
sachital <sachinmaurya1710@gmail.com>
2023-02-11 11:57:25 +0530

    modified operators

---
+++ 2. Operators/Assignment.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Assignment {
+    public static void main(String[] args) {
+        Scanner sy = new Scanner(System.in);
+        int a;
+        int b=5;
+        a=b;
+        a+=b; // is same as a=a+b;
+        System.out.println(a);
+        a-=b; // is same as a=a-b;
+        System.out.println(a);
+        a*=b; // is same as a=a*b;
+        System.out.println(a);
+        a/=b; // is same as a=a/b;
+        System.out.println(a);
+        a%=b; // is same as a=a%b;
+        System.out.println(a);
+    }
+}
