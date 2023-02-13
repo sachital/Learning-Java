9d98a57a84365640e3fc1b4934fb69a37289353f
sachital <sachinmaurya1710@gmail.com>
2023-02-13 12:40:10 +0530

    added loops

---
+++ 4. Loops/for/squarepattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class squarepattern {
+    public static void main(String[] args) {
+        Scanner sy = new Scanner(System.in);
+        System.out.println("Enter the context");
+        int c = sy.nextInt();
+        System.out.println("Enter the number of rows");
+        int n = sy.nextInt();
+        for(int i=1;i<=n;i++){
+            for()
+        }
+    }    
+}
