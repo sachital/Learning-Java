f3573d9e0025d08db071695e4a07a897c28d8287
sachital <sachinmaurya1710@gmail.com>
2023-02-23 22:30:18 +0530

    Pattern advanced

--- 5. Patterns/rotatedhhalfoyramid.java
+++ 5. Patterns/rotatedhhalfoyramid.java
@@ -1,8 +1,28 @@
 import java.util.*;
 
 public class rotatedhhalfoyramid {
+
+    public static void rotatedhpy(int n){
+
+        for( int i=1; i<=n;i++){
+
+            for(int j=1; j<=(n-i);j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
     
     public static void main(String[] args) {
-        
+        Scanner sy = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int x = sy.nextInt();
+
+        rotatedhpy(x);
     }    
 }
