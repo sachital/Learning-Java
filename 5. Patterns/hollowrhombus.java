b1ac6be3925391c16ea86bd617979eeb1377308b
sachital <sachinmaurya1710@gmail.com>
2023-03-02 22:22:11 +0530

    hollow rhombus

---
+++ 5. Patterns/hollowrhombus.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class hollowrhombus {
+
+    public static void hollow(int n){
+
+        for ( int i=1; i<=n;i++ ){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+           
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sy = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int x = sy.nextInt();
+        hollow(x);
+    }
+}
