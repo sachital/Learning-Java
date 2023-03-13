cfae4a0b3f355f167f4be832cc2465a035c189be
sachital <sachinmaurya1710@gmail.com>
2023-03-13 11:51:03 +0530

    arrays

---
+++ 7. Arrays/inputoutput.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class inputoutput {
+    public static void main(String[] args) {
+
+        Scanner sy= new Scanner(System.in);
+
+        int marks[]= new int[10];
+
+        /*marks[0]= sy.nextInt();
+        marks[1]= sy.nextInt();
+        marks[2]= sy.nextInt();
+
+        System.out.println("subject 1: "+ marks[0]);
+        System.out.println("subject 2: "+ marks[1]);
+        System.out.println("subject 3: "+ marks[2]);
+        */
+
+        System.out.println("Lenght of array = "+ marks.length);//finding lenght of array
+        
+    }
+}
