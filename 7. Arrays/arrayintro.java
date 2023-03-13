a88de101f5e8b44c9b20d24907b71a37be3f3e18
sachital <sachinmaurya1710@gmail.com>
2023-03-13 11:53:38 +0530

    resolved String syntax

--- 7. Arrays/arrayintro.java
+++ 7. Arrays/arrayintro.java
@@ -2,8 +2,8 @@ public class arrayintro {
     public static void main(String[] args) {
         int subject[] = new int[45];//array size 45 (blank)
 
-        int number[]= {1,2,4,6};//size 4(having elements)
+        int number[]= {1,2,4,6};//size 4(having elements )
 
-        string name []= {"appu","deepak","raja"};// size 3
+        String name[]= {"appu","deepak","raja"};// size 3
     }
 }
