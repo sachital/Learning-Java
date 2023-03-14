5acb3a3a095db8ea55cbef76487814135fae5cf0
such-in <sachinmaurya1710@gmail.com>
2023-03-14 22:23:56 +0530

    sub arrays

--- 7. Arrays/subarrays.java
+++ 7. Arrays/subarrays.java
@@ -5,16 +5,17 @@ public class subarrays {
     }
     
     public static void sbarrays(int numbers[]){
+        int ts=0;
         for(int i= 0;i<numbers.length;i++){
             for(int j=i;j<numbers.length;j++){
                 for(int k=i;k<j;k++){// only for print
-                    System.out.print(+ numbers[k]+"");
+                    System.out.print(+ numbers[k]+" ");
                 }
+                ts++;
                 System.out.println();
             }
-            
             System.out.println();
-        }
+        }System.out.println("total sub-arrays "+ ts);
         
     }
 }
